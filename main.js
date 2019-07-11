28328fe1713e8fe779489eade23e1b78370ecf44
Álvaro Luis Vieira Quevedo Júnior <alvaroquevedo@live.com>
2019-07-11 16:26:14 -0300

    added window

--- main.js
+++ main.js
@@ -10,37 +10,41 @@ const Tray = electron.Tray
 const iconPath = path.join(__dirname, 'ytlogo.jpg')
 let tray = null
 
-app.on('ready', function(){
+function createWindow() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html')
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  })
+}
+
+app.on('ready', function () {
   tray = new Tray(iconPath)
 
   let template = [
     {
-      label: 'Audio',
-      submenu: [
-        {
-          label: 'Low',
-          type: 'radio',
-          checked: true
-        },
-        {
-          label: 'High',
-          type: 'radio',
-        }
-      ]
-    },
-    {
-      label: 'Video',
-      submenu: [
-        {
-          label: '1280x720',
-          type: 'radio',
-          checked: true
-        },
-        {
-          label: '1920x1080',
-          type: 'radio',
-        }
-      ]
+      label: 'Statistics',
+      click: function()
+      { 
+        createWindow()
+      }
     }
   ]
 
